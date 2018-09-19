2831e24b88d8b1c2d4065943f774d9c9ed6a021d
Aris Ripandi <ar.is@outlook.com>
2018-09-20 01:48:52 +0700

    Some changes

--- build.sh
+++ build.sh
@@ -10,13 +10,44 @@
 perl -pi -e 's#(SELINUX=)(.*)#${1}permissive#' /etc/selinux/config
 
 # Install the toolchain
-dnf -y install kernel-modules-$(uname -r) mock curl livecd-tools pungi {fedora,spin,l10n}-kickstarts
+dnf -y install kernel-modules-$(uname -r) mock curl livecd-tools pungi \
+ {fedora,spin,l10n}-kickstarts pykickstart anaconda lorax virt-install \
+  libvirt-daemon-config-network
+
 usermod -a -G mock ariss
 
 # Init Environment: run as general user
-mock -r fedora-27-x86_64 --init
-mock -r fedora-27-x86_64 --install lorax-lmc-novirt vim-minimal pykickstart
+mock -r fedora-28-x86_64 --init
+mock -r fedora-28-x86_64 --install lorax-lmc-novirt vim-minimal pykickstart
 
+# Get kickstarts
+git clone https://pagure.io/fedora-kickstarts.git fedora-kickstarts -b f28
+
+# Running a Compose
+mock -r fedora-28-x86_64 --shell --old-chroot
+
+# Chroot: Flatten a Kickstart
+ksflatten --config fedora-live-xfce.ks -o flat-fedora-live-xfce.ks --version F28
+
+# Chroot: Create the Live Image
+mkdir -p /{var,tmp}/livebuild
+livemedia-creator \
+ --ks ertix-live-budgie.ks \
+ --logfile /tmp/livebuild.log \
+ --tmp /tmp/livebuild \
+ --resultdir /var/livebuild \
+ --project "Fedora Budgie Remix"  \
+ --volid "Fedora Budgie Remix x64" \
+ --title "Fedora Budgie Remix" \
+ --make-iso --iso-only --releasever 28 \
+ --iso-name fedora-budgie-live.iso \
+ --no-virt --macboot
+
+# Compose Results and cleanup
+cp /var/lib/mock/fedora-27-x86_64/root/var/lmc/*.iso $HOME
+mock -r fedora-27-x86_64 --clean
+
+# Old method
 mkdir -p kickstart.d ; cd kickstart.d
 curl -sLO https://pagure.io/fedora-kickstarts/raw/f28/f/fedora-repo.ks
 curl -sLO https://pagure.io/fedora-kickstarts/raw/f28/f/fedora-live-base.ks
@@ -31,17 +62,3 @@ mv fedora-xfce-common.ks remix-budgie-common.ks
 
 # In the %post section of the kickstart file, add the following as the first line:
 # sed -i -e ‘s/Generic release/Budgie Fedora Remix/g’ /etc/fedora-release /etc/issue
-
-rm -fr /var/cache/live
-
-livemedia-creator \
- --ks ertix-live-budgie.ks \
- --logfile /tmp/livebuild.log \
- --resultdir /tmp/livebuild \
- --project "Fedora Budgie Remix"  \
- --volid "Fedora Budgie Remix x64" \
- --title "Fedora Budgie Remix"
- --make-iso --iso-only --releasever 28 \
- --iso-name fedora-budgie-live.iso \
- --no-virt --macboot
-
